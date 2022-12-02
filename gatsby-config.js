8b58ce72056fe1d984b7e4e825d6778640430b82
George Butler <georgebutleriv@gmail.com>
2022-12-02 15:35:13 -0800

    Portfolio

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,12 +9,13 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `George Butler`,
+    description: `Web Developer`,
+    author: `@georgelbutler`,
+    siteUrl: `https://georgebutler.dev`,
   },
   plugins: [
+    `gatsby-plugin-postcss`,
     `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
